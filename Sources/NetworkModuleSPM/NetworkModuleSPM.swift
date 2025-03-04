66b363d7eb9d231fe3ded1a29b3d9af6ed6d7bca
Ram <ram2386@gmail.com>
2025-03-04 23:39:40 +0530

    New update

--- Sources/NetworkModuleSPM/NetworkModuleSPM.swift
+++ Sources/NetworkModuleSPM/NetworkModuleSPM.swift
@@ -1,2 +1,14 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+import NetworkFramework
+
+public class ImplementNetwork1 {
+    public init() {}
+    
+    public func callNetwork() -> String {
+//        let network = NetworkFramework()
+        return "Hello, World!"
+    }
+}
